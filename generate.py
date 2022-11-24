2fcc3a08b7b36d7f6735188fc2a34918ef06a25d
Jacob <73615055+jacobdev03@users.noreply.github.com>
2022-11-24 13:59:20 +0100

    open wordlist func

--- generate.py
+++ generate.py
@@ -5,8 +5,15 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Generate usernames using python")
 parser.add_argument(
-    '-w', '--wordlist', help="Specify wordlist with first and last name")   
+    '-w', '--wordlist', help="Specify wordlist with first and last name")
 
 args = parser.parse_args()
 
 wordlist_name = args.wordlist
+
+names = []
+
+with open(f"{wordlist_name}") as file:
+    for line in file.readlines():
+        names.append(line.rstrip("\n"))
+
