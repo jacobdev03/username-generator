07b95973d46efba95cf5b76a3cdbfdc307a232ce
Jacob <73615055+jacobdev03@users.noreply.github.com>
2022-11-24 14:27:21 +0100

    generate usernames function

--- generate.py
+++ generate.py
@@ -17,3 +17,47 @@ with open(f"{wordlist_name}") as file:
     for line in file.readlines():
         names.append(line.rstrip("\n"))
 
+def generate_usernames():
+    first_last = []
+    first_last_upper = []
+    for n in names:
+        first_last.append(n.lower().split())
+        first_last_upper.append(n.upper().split())
+        
+
+    for name in first_last:
+        print(name[0])
+        print(name[1])
+        print(f"{name[0]}{name[1]}")
+        print(f"{name[1]}{name[0]}")
+        print(f"{name[0]}-{name[1]}")
+        print(f"{name[0]}.{name[1]}")
+        print(f"{name[0]}_{name[1]}")
+        print(f"{name[0]}+{name[1]}")
+        print(f"{name[0][0]}{name[1]}")
+        print(f"{name[0][0]}-{name[1]}")
+        print(f"{name[0][0]}.{name[1]}")
+        print(f"{name[0][0]}_{name[1]}")
+        print(f"{name[0][0]}+{name[1]}")
+
+
+    print("---------UPPERCASE-----------")    
+
+
+    for name in first_last_upper:
+        print(name[0])
+        print(name[1])
+        print(f"{name[0]}{name[1]}")
+        print(f"{name[1]}{name[0]}")
+        print(f"{name[0]}-{name[1]}")
+        print(f"{name[0]}.{name[1]}")
+        print(f"{name[0]}_{name[1]}")
+        print(f"{name[0]}+{name[1]}")
+        print(f"{name[0][0]}{name[1]}")
+        print(f"{name[0][0]}-{name[1]}")
+        print(f"{name[0][0]}.{name[1]}")
+        print(f"{name[0][0]}_{name[1]}")
+        print(f"{name[0][0]}+{name[1]}")
+
+
+generate_usernames()
