23554663485fc56bb8ac0b61c78e0ed125a64c71
Jacob <73615055+jacobdev03@users.noreply.github.com>
2022-11-24 13:46:38 +0100

    arg parsing functionality

--- generate.py
+++ generate.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python
 
 import argparse
+
+
+parser = argparse.ArgumentParser(description="Generate usernames using python")
+parser.add_argument(
+    '-w', '--wordlist', help="Specify wordlist with first and last name")   
+
+args = parser.parse_args()
+
+wordlist_name = args.wordlist
